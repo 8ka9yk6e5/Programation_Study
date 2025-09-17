c6c0c8e3dae973801c547153685116c7c6ead668
Kayke <kaykekk8965@gmail.com>
2025-09-17 14:50:29 -0300

    first project learning JS, complete

---
+++ 1st_project_user_record.js
@@ -0,0 +1,27 @@
+//variables
+
+let name;
+let age;
+let height;
+let isStudying;
+
+//Entering the informations
+
+name = "Kayke K. Krevellin";
+age = 16;
+height = 1.70;
+isStudying = true;
+
+//showing the variables in a table
+
+console.log(`Name: ${name}`);
+console.log(`Value type: ${typeof name}\n`);
+
+console.log(`Age: ${age}`);
+console.log(`Value type: ${typeof age}\n`);
+
+console.log(`Height: ${height}`);
+console.log(`Value type: ${typeof height}\n`);
+
+console.log(`User studying yet: ${isStudying}`);
+console.log(`Value type: ${typeof isStudying}\n`);
