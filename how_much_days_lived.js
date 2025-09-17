00f7e8772ce8a6b4e920a1fd8b2ab75c1f50da04
Kayke <kaykekk8965@gmail.com>
2025-09-17 15:14:12 -0300

    project 2, to calculate how much days a people lived

---
+++ how_much_days_lived.js
@@ -0,0 +1,12 @@
+const name = "Kayke K. Krevellin";
+const age = 16;
+const daysLived = 365 * age;
+
+console.log(`Name: ${name}`);
+console.log(`Value type: ${typeof name}\n`);
+
+console.log(`Age: ${age}`);
+console.log(`Value type: ${typeof age}\n`);
+
+console.log(`Days lived (approx.): ${daysLived}`);
+console.log(`Value type: ${typeof daysLived}`);
