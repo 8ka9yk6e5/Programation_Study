b2ae7b5a42ee123b479ecd4a082593f94a3da7cf
Kayke <kaykekk8965@gmail.com>
2025-09-22 19:35:49 -0300

    another project, a simple calculator

---
+++ Simple_calculator.js
@@ -0,0 +1,13 @@
+let numberOne, numberTwo, operator, result;
+
+numberOne = 89; //change the number to test
+
+numberTwo = 65; //also change the number to test
+
+//if change the type of calculus change both of them
+
+operator = "-";
+
+result = numberOne - numberTwo;
+
+console.log(`${numberOne} ${operator} ${numberTwo} = ${result}`);
