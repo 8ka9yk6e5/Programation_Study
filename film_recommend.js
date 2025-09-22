f478ef1538748add0eec1d8436df3661a7cba77d
Kayke <kaykekk8965@gmail.com>
2025-09-22 14:19:38 -0300

    another simple project

---
+++ film_recommend.js
@@ -0,0 +1,3 @@
+const filmName = "Avengers", note = 8.5, recommend = true;
+
+console.log(`Film: ${filmName}(type: ${typeof filmName})\nNote: ${note}(type: ${typeof note})\nRecommend: ${recommend}(type: ${typeof recommend})`);
