f7064814bdc3c6bf621e4185d38b5b8288e3ed20
Kayke <kaykekk8965@gmail.com>
2025-09-22 16:47:58 -0300

    made some tests

--- test1.js
+++ test1.js
@@ -1,1 +1,8 @@
-console.log(`hello`)
+console.time("test");
+console.group();
+console.log("this is inside a group");
+console.groupEnd();
+console.group("another");
+console.log("another group");
+console.groupEnd();
+console.timeEnd("test");
