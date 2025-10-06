4bfe55801253037ea48758843c1719b22c9403ec
Kayke <kaykekk8965@gmail.com>
2025-10-06 19:08:16 -0300

    Is a new simple project to create random passwords

--- Password_gerator.js
+++ Password_gerator.js
@@ -1,5 +1,4 @@
-//get a enter to user to get how much digits to has the password
-//create a form to remove the comma
+const readline = require('node:readline');
 
 let digits = 12;
 
@@ -15,8 +14,6 @@ let numberRadomizer = () => Math.floor(Math.random()*10);
 let letterRandomizer = () => {
     const num = Math.floor(Math.random() * (26 - 0) + 0);
     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    
-    if (num > alphabet.length) console.log("ERROR - value out of array");//for precaution
 
     return alphabet[num];
 };
@@ -25,14 +22,28 @@ function transfer(callbackWhich, callbackLetter, callbackNumber){
     return callbackWhich() ? callbackLetter() : callbackNumber();
 }
 
-function creatingPassword(timesToRepeat = 6){
+function creatingPassword(timesToRepeat){
     let password = [];
 
-    for (i = 0; i < timesToRepeat; i++){
+    for (let i = 0; i < timesToRepeat; i++){
         password.push(transfer(numberOrLetter, letterRandomizer, numberRadomizer));
     }
 
-    return String(password);
+    return String(password).replaceAll(',', '');//remove the commas and create the password in a correct format
 }
 
-console.log(creatingPassword(10));
+function showPassword(digits = 12){
+    console.clear();
+    console.log(`How much digits: ${digits}`);
+    console.log(`\nPassword: ${creatingPassword(digits)}\n`);
+}
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+rl.question("How much digits this password need has?\n", (digits) => {
+    showPassword(Number(digits));
+    rl.close();
+});
