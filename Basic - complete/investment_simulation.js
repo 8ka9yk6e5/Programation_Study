3bf5e5b084e88ecf4bb3a43dca9717e6eb8f4efd
Kayke <kaykekk8965@gmail.com>
2025-10-10 20:57:10 -0300

    some edits and a add of new functions

--- Basic - complete/investment_simulation.js
+++ Basic - complete/investment_simulation.js
@@ -1,17 +1,32 @@
 const readline = require('node:readline');
 
 const investmentInformation = {
-    taxInPeriod : null,
-    time : null,
-    interest(startValue) {return startValue * this.taxInPeriod *this.time},//used when don't have a acumulated interest, used in short time
-    compoundInterest(startValue) {return startValue * (1 + this.taxInPeriod) ** this.time}//with acumulated interest, used in medium or long time
+    taxInPeriod : undefined,
+    time : undefined,
+    interest(startValue) {return (startValue * this.taxInPeriod *this.time)},//used when don't have a acumulated interest, used in short time
+    compoundInterest(startValue) {return (startValue * (1 + this.taxInPeriod) ** this.time)}//with acumulated interest, used in medium or long time
 };
 
-let Equity, income;
+const date = {
+    day : new Date().getDate(),
+    month : new Date().getMonth(),
+    year : new Date().getFullYear()
+}
 
-const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout
-})
+let Equity;
 
-rl.question("this working?", (enter) => {console.log(enter) })
+function typeOfInvestment(){
+    const rl = readline.Interface({
+        input : process.stdin,
+        output : process.stdout
+    })
+
+    rl.question(`(response with number)\nYour investment is for a:\n1-long period\n2-medium period\n3-short period\n`, (userEnter) => {
+        if (userEnter == 1 || userEnter == 2) 
+        else 
+    })
+}
+
+function getInformationsOfInvestment(){
+
+}
