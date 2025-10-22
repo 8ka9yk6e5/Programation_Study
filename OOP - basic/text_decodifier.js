a9aab1ee68ea3a89ae75bc9e8b953de6bfc37826
Kayke <kaykekk8965@gmail.com>
2025-10-22 17:37:08 -0300

    creating the codificating text method, has a bug and a bad logic - will be correct

--- OOP - basic/text_decodifier.js
+++ OOP - basic/text_decodifier.js
@@ -1,16 +1,30 @@
 const readline = require("readline");
 
-const gettingText = {
-    text : null,
-    getText() {
-        const rl = readline.createInterface({
-            input : process.stdin,
-            output : process.stdout
-        })
+function getText(callback) {
+    const rl = readline.createInterface({
+        input : process.stdin,
+        output : process.stdout
+    })
 
-        rl.question("Enter the text to codificate them:\n", (userEnter) =>{
-            this.text = userEnter;
-            rl.close();
-        })
+    rl.question("Enter the text to codificate them:\n", (userEnter) => {
+        rl.close();
+        callback(userEnter);
+    })
+}
+
+const codificatingText = {
+    codificate : [],
+    roundCodificate : [],
+    codeInStringForm : "",
+    codificating(text){
+        for(let i = 0; i < text.length; i++){
+            let code = text.codePointAt(i).toString();
+            this.codificate.push(code * 0.5);
+        }
+        this.roundCodificate = this.codificate.forEach(item => Math.round(item));
+        this.codeInStringForm = this.roundCodificate.join(' ');
+        console.log(this.codeInStringForm);
     }
 }
+
+getText((userEnterText) => codificatingText.codificating(userEnterText));
