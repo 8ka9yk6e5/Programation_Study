475b8579a4cfa8f0a938c916ae1d8f45faeff152
Kayke <kaykekk8965@gmail.com>
2025-10-27 16:16:15 -0300

    codificate and show to user is complete

--- OOP - basic/text_decodifier.js
+++ OOP - basic/text_decodifier.js
@@ -15,14 +15,22 @@ function getText(callback) {//function to user enter the text
 const codificatingText = {//object with the form to codificate
     codificate : [],//text codificate
     codeInStringForm : "",//codefication to show
+    roundCodificate : [],
     codificating(text){
         for(let i = 0; i < text.length; i++){//to pass all character and transform each in a codification
             let code = text.codePointAt(i).toString();//transform the specificy character in a code
             this.codificate.push(code * 0.5);//to get more codificate, get half of them
         }
-        const roundCodificate = this.codificate.forEach(item => Math.round(item));//round each value to show
-        this.codeInStringForm = roundCodificate.join(' ');//join to a string with all codes
+        this.roundCodificate = this.codificate.map(item => Math.round(item));//round codificate transform the float values into a normal number
+        this.codeInStringForm = this.roundCodificate.join(' ');//join to a string with all codes
+        showText.show(this.codeInStringForm);
     }
 }
 
-getText((userEnterText) => codificatingText.codificating(userEnterText));//test
+const showText = {
+    show(textCode){
+        console.log(`codificate text:\n${textCode}`)
+    }
+}
+
+getText((userText) => codificatingText.codificating(userText));//test
