863c061f2ee2e663a55ac7dc1f2b1a65cd9e7ca7
Kayke <kaykekk8965@gmail.com>
2025-10-27 16:25:23 -0300

    decotificate is complete and working

--- OOP - basic/text_decodifier.js
+++ OOP - basic/text_decodifier.js
@@ -14,8 +14,8 @@ function getText(callback) {//function to user enter the text
 
 const codificatingText = {//object with the form to codificate
     codificate : [],//text codificate
-    codeInStringForm : "",//codefication to show
     roundCodificate : [],
+    codeInStringForm : "",//codefication to show
     codificating(text){
         for(let i = 0; i < text.length; i++){//to pass all character and transform each in a codification
             let code = text.codePointAt(i).toString();//transform the specificy character in a code
@@ -23,12 +23,22 @@ const codificatingText = {//object with the form to codificate
         }
         this.roundCodificate = this.codificate.map(item => Math.round(item));//round codificate transform the float values into a normal number
         this.codeInStringForm = this.roundCodificate.join(' ');//join to a string with all codes
-        showText.show(this.codeInStringForm);
+        decodificatingText.decodificating(this.codificate);
+        showText.showCodefication(this.codeInStringForm);
+    }
+}
+
+const decodificatingText= {
+    decodificate : "",
+    normalCode : [],
+    decodificating(code){
+        this.normalCode = code.map(item => item * 2);
+        this.decodificate = this.normalCode.map(item => String.fromCodePoint(item)).toString();
     }
 }
 
 const showText = {
-    show(textCode){
+    showCodefication(textCode){
         console.log(`codificate text:\n${textCode}`)
     }
 }
