b82086d2b7a81285d4ab9d82f1551df528e73279
Kayke <kaykekk8965@gmail.com>
2025-10-29 17:25:17 -0300

    creation of random number and verification with user enter

--- OOP - basic/number_guess.js
+++ OOP - basic/number_guess.js
@@ -1,2 +1,9 @@
-const readline = require("node.js:readline");
+const readline = require("readline");
 
+const randomNumber = Math.round(Math.random()*100);//number to user descovery
+
+function comparingNumber(userNum = 0, callback) {
+    if(userNum > randomNumber) console.log("Number is higher than enter number");
+    else if (userNum < randomNumber) console.log("Number is lower than enter number");
+    else callback();
+}
