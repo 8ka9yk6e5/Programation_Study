470ae5d647d9e3e6c6eb20fa32b0750dc62f0709
Kayke <kaykekk8965@gmail.com>
2025-10-29 20:32:39 -0300

    part of the life system is maked

--- OOP - basic/number_guess.js
+++ OOP - basic/number_guess.js
@@ -6,4 +6,16 @@ function comparingNumber(userNum = 0, callback) {
     if(userNum > randomNumber) console.log("Number is higher than enter number");
     else if (userNum < randomNumber) console.log("Number is lower than enter number");
     else callback();
+};
+
+const lifeSystem = {//object referend to lose life system
+   lifes : 5,
+   losedLifes : 0,
+    losingLife(){
+        this.losedLifes++;
+        if (this.losedLifes === this.lifes){
+            console.clear();
+            console.log("YOU LOSE!");
+        }
+    }
 }
