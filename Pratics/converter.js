0a8499b87f4647ad37bff4ad04a03008eb3a8c73
Kayke <kaykekk8965@gmail.com>
2025-11-17 14:57:52 -0300

    project converter started

---
+++ Pratics/converter.js
@@ -0,0 +1,17 @@
+/*
+an API test:
+-try to create a complete code using a APIU like work
+-an API wich convert anything:
+    -temperature
+    -volume
+    -money
+    -mass
+    -speed
+    -time
+    -energy
+    -power
+    -storage
+    -pression
+    -lenght
+*/
+
