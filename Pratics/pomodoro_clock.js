322f4eeb01cd1784162cf87b050decf610bacab4
Kayke <kaykekk8965@gmail.com>
2025-11-24 17:17:41 -0300

    method to get the time of set is complete

--- Pratics/pomodoro_clock.js
+++ Pratics/pomodoro_clock.js
@@ -1,1 +1,35 @@
-//creation of a pomodoro clock only using code, not an API
+//creation of a pomodoro clock only using code, without an API
+
+const readline = require('node:readline');
+const {stdin : input, stdout : output} = require('node:process');
+
+const gettingInformations = {
+    timeOfSet : 0,
+    timeOfRest : 0,
+    Sets : 0,
+    getTimeOfSet(){
+        const rl = readline.createInterface({input, output});
+
+        rl.question('(in minutes)How much time be each set?\n', (userEnter) => {
+            if (!isNaN(Number(userEnter)) && userEnter > 0) {
+                this.timeOfSet = Number(userEnter);
+                rl.close();
+            }
+            else {
+                console.log("ERROR - invalid value");
+                rl.close();
+                this.getTimeOfSet();
+            }
+            
+        });
+        
+    },
+    getTimeOfRest(){
+        const rl = readline.createInterface({input, output});
+    },
+    getQuantityOfSets(){
+        const rl = readline.creationInterface({input, output});
+    }
+}
+
+gettingInformations.getTimeOfSet();
