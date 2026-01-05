909a265f53cdd8140162cd84b7fd457d8cb5a737
k9a8y6k5e <kaykekk8965@gmail.com>
2026-01-05 17:07:58 -0300

    method which save the student is complete

--- practice/APIs/notes_API.js
+++ practice/APIs/notes_API.js
@@ -23,13 +23,49 @@ const studentCreationService = {
         informations = studentsCreationValidator.unimportantValueRemover(Object.entries(informations));
 
         if(studentsCreationValidator.studentCreationImportantValueVerificador(Object.keys(informations))){
-            if(studentsCreationValidator.studentCreationValidatorControl(Object.entries(informations))){
+            if(this.studentCreationValidatorControl.call(studentsCreationValidator, Object.entries(informations))){
+                this.studentsCreationSaver(informations);
                 return true;
             }
             else return false;
         }
         else return false;
+    },
+
+    //methods which add a setter inside each value
+    studentCreationValidatorControl(pairs){
+        let name, grade, absences, notes;
+        for(let pair of pairs){
+            switch(pair[0]){
+                case 'name':
+                    name = this.studentCreationNameValidator(pair[1]);
+                    break;
+                case 'grade':
+                    grade = this.studentCreationGradeValidator(pair[1]);
+                    break;
+                case 'absences':
+                    absences = this.studentCreationAbsencesValidator(pair[1]);
+                    break;
+                case 'notes':
+                    notes = this.studentCreationNotesValidator(Object.entries(pair[1]));
+                    break;
+            }
+        }
+
+        return (name && grade && absences && notes);
+    },
+
+    studentsCreationSaver(informationReq){
+        this.studentObjectValues = new objectConstructor(informationReq);
+        const {name : studentObjectKey} = informationReq;
+        studentsMap.set(studentObjectKey, this.studentObjectValues);
     }
+};
+
+function objectConstructor(originalObj){
+        this.grade = originalObj.grade;
+        this.absences = originalObj.absences;
+        this.notes = originalObj.notes;
 };
 
 const studentsCreationValidator = {
@@ -62,29 +98,6 @@ const studentsCreationValidator = {
         //add an error
     },
 
-    //methods which add a setter inside each value
-    studentCreationValidatorControl(pairs){
-        let name, grade, absences, notes;
-        for(let pair of pairs){
-            switch(pair[0]){
-                case 'name':
-                    name = this.studentCreationNameValidator(pair[1]);
-                    break;
-                case 'grade':
-                    grade = this.studentCreationGradeValidator(pair[1]);
-                    break;
-                case 'absences':
-                    absences = this.studentCreationAbsencesValidator(pair[1]);
-                    break;
-                case 'notes':
-                    notes = this.studentCreationNotesValidator(Object.entries(pair[1]));
-                    break;
-            }
-        }
-
-        return (name && grade && absences && notes);
-    },
-    
     //method to verify if the name is in a correct format
     studentCreationNameValidator(name){
         if(name?.trim().length > 0 ) return true;
@@ -113,7 +126,6 @@ const studentsCreationValidator = {
 app.listen(3001);
 
 //when learn about error handling add the errors
-//add a saver of the student
 //add a note verification if has medium note, and if doesn't add a form to calculate and add
 //add a search student method
 //add a delete method to remove student
