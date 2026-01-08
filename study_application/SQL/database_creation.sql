dcd4d27453047d767a794d021a1d11a5654c0ba8
k9a8y6k5e <kaykekk8965@gmail.com>
2026-01-08 14:08:20 -0300

    study about SQL, with a progess, studying the creation and basic use of databases and tables

---
+++ study_application/SQL/database_creation.sql
@@ -0,0 +1,15 @@
+-- Active: 1767884026646@@localhost@3307
+CREATE DATABASE studyDB;
+--CREATE : create a thing, which is specified after the command
+--DATABASE : specify the use to a database
+
+USE studyDB;
+--USE : specify which database/table will be used
+
+DROP DATABASE studyDB;
+--DROP : delete a specified data and value
+
+ALTER DATABASE studyDB READ ONLY = 0;
+--ALTER : modify things in specified data and value
+--READ ONLY : transform the dabase/table in a read only mode, impossibiling editing the values
+-- if READ ONLY equals to 0 it's disable, if 1 it's able
---
+++ study_application/SQL/table_creation.sql
@@ -0,0 +1,65 @@
+USE studyDB;
+--always use "USE" to specfy where it happens
+
+CREATE TABLE person (
+    person_id INT,
+    person_name VARCHAR(50),
+    person_age TINYINT
+);
+
+-- TABLE : specify to a table
+-- inside the "()" need to had the columns which the table will be have
+-- at each value side, need to add a data type
+
+SELECT * FROM person;
+
+-- SELECT : extracts a data from a database
+-- * : select all values
+-- FROM : specify from where it be removed
+
+RENAME TABLE person TO users; 
+
+-- RENAME : to change the name of a data
+-- TO : to new name
+
+
+DROP TABLE users;
+
+
+ALTER TABLE users
+    ADD phone_number VARCHAR(15);
+
+-- ADD : add a new column to the table
+
+SELECT * FROM users;
+
+
+ALTER TABLE users
+    RENAME COLUMN person_id TO users_id,
+    RENAME COLUMN person_name TO users_name,
+    RENAME COLUMN person_age TO users_age;
+
+-- RENAME COLUMN : change the name of the column
+
+ALTER TABLE users
+    MODIFY COLUMN users_id SMALLINT;
+
+-- MODIFY COLUMN : modify the data type of the column
+
+
+ALTER TABLE users
+    MODIFY users_id SMALLINT
+    AFTER users_name;
+
+-- AFTER : move the column to side of/after another one
+
+ALTER TABLE users   
+    MODIFY users_id SMALLINT
+    FIRST;
+
+-- FIRST :  move the column to this be the first column
+
+ALTER TABLE users
+    DROP COLUMN phone_number;
+
+-- DROP COLUMN : delete an specified column
