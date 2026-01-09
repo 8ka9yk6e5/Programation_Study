5bb82a82f5bbb0fadfca61083c5d9fac9235c8a9
k9a8y6k5e <kaykekk8965@gmail.com>
2026-01-09 17:22:31 -0300

    creation of codeValidator

--- practice/APIs/Stock_API/item_validator.js
+++ practice/APIs/Stock_API/item_validator.js
@@ -2,8 +2,34 @@ const itemValidator = {
     keyValidator(req, res, next){
         const keys = Object.keys(req.body);
         if(keys.every(value => value.trim().length > 0)) next();
-        else throw new Error('Invalid Key in values').message;
-    }
+        else throw new Error('Invalid Key in request').message;
+    },
+
+    valueValidator(req, res, next){
+        try{
+            for(let item of Object.entries(req.body)){
+                let itemObject = Object.fromEntries(item[1]);
+                if (itemValidator._codeValidator(itemObject.code)) res.end();
+            }
+        }
+        catch(err){
+            res.status(400).json({work : false, Error : err.message});
+        }
+    },
+
+    _codeValidator(code){
+        const strCode = String(code);
+        if(strCode.length == 6 && !isNaN(code)) return true;
+        else throw new Error('Invalid code value'); 
+    },
+
+    _quantityValidator(quantity){
+
+    },
+
+    _priceValidator(price){
+
+    },
 };
 
 module.exports = itemValidator;
--- practice/APIs/Stock_API/routes.js
+++ practice/APIs/Stock_API/routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {keyValidator} = require('./item_validator');
+const {keyValidator, valueValidator} = require('./item_validator');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/item/add', keyValidator);
+app.post('/item/add', keyValidator, valueValidator);
 
 app.listen(3001);
